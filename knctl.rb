033cb817ed150013fbca40a8517d1fa0fe703d9d
Dr Nic Williams <drnicwilliams@gmail.com>
2019-03-23 08:48:39 +1100

    Revert "Updated knctl.rb from new release" until https://github.com/cppforlife/knctl/issues/63
    This reverts commit 4aee32d493c47316b4126e03a48d0e4eb4c8ec6a.

--- knctl.rb
+++ knctl.rb
@@ -3,10 +3,10 @@ require "FileUtils"
 class Knctl < Formula
   homepage "https://github.com/cppforlife/knctl"
 
-  v = "v0.2.0" # CI Managed
+  v = "v0.1.0" # CI Managed
   url "https://github.com/cppforlife/knctl/releases/download/#{v}/knctl-darwin-amd64"
   version v
-  sha256 "" # CI Managed
+  sha256 "e98d9931bca05dd545c25acddc1366fa78cb3b5bacb5110b59fefa0c1fb3f0a5" # CI Managed
 
   def install
     FileUtils.mv("knctl-darwin-amd64", "knctl")
